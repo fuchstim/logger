cfca77ec4c5cfb41efacde7101ba7c8d353522d6
Tim Fuchs <github@gp.ftim.eu>
2023-07-22 16:03:35 -0400

    Create logger package

---
+++ logger/src/index.ts
@@ -0,0 +1,75 @@
+import util from 'util';
+
+type TLoggerOptions = {
+  prefix: string;
+  color: boolean;
+  transport: TLogTransport;
+};
+type TLogFragment = string | number | symbol | boolean | null | undefined | Error | object;
+type TLogLevel = 'debug' | 'info' | 'warn' | 'error';
+
+type TLogTransport = Record<TLogLevel, (message: string) => void>;
+
+export class Logger {
+  private options: TLoggerOptions;
+
+  constructor(options?: Partial<TLoggerOptions>) {
+    this.options = {
+      ...options,
+
+      color: true,
+      prefix: '',
+      transport: console,
+    };
+  }
+
+  ns(...namespaces: string[]) { return this.namespace(...namespaces); }
+
+  namespace(...namespaces: string[]) {
+    return new Logger({
+      ...this.options,
+      prefix: [
+        this.options.prefix,
+        ...namespaces.map(ns => `[${ns}]`),
+      ].join('').trim(),
+    });
+  }
+
+  log(...args: TLogFragment[]): void;
+  log(level: TLogLevel, ...args: TLogFragment[]) {
+    this[level ?? 'info'](...args);
+  }
+
+  error(...args: TLogFragment[]) { this.writeLog('error', args); }
+  warn(...args: TLogFragment[]) { this.writeLog('warn', args); }
+  info(...args: TLogFragment[]) { this.writeLog('info', args); }
+  debug(...args: TLogFragment[]) { this.writeLog('debug', args); }
+
+  protected writeLog(level: TLogLevel, fragments: TLogFragment[]) {
+    const formattedFragments = [ this.options.prefix, ...fragments, ]
+      .map(fragment => {
+        if ([ 'string', 'boolean', 'number', ].includes(typeof fragment)) {
+          return String(fragment);
+        }
+
+        return util.inspect(
+          fragment,
+          {
+            showHidden: false,
+            depth: 5,
+            colors: this.options.color,
+          }
+        );
+      })
+      .filter(fragment => Boolean(fragment.length));
+
+    const message = [ `[${level}]`, ...formattedFragments, ].join(' ');
+
+    this.options.transport[level].apply(
+      this.options.transport,
+      [ message, ]
+    );
+  }
+}
+
+export default new Logger();
